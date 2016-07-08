7dfaee6cd8cb50ce2f3e1ba33c1cff7e9ee08961
BrunoMessias <brunodsm10@gmail.com>
2016-07-08 05:10:21 -0300

    Pacote "escritor" finalizado e testado

--- src/main/java/ExtrairAtividades.java
+++ src/main/java/ExtrairAtividades.java
@@ -1,22 +1,26 @@
 import ms.analisador.GenericParser;
+import ms.escritor.ConfeccionaDocumento;
+import ms.manipulador.ModeloAtividade;
+import org.joda.time.DateTime;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Tiago on 06/07/2016.
  */
 public class ExtrairAtividades {
-    public static void main(String args[]) {
-        File input = new File("D:\\input.pdf");
+    public static void main(String args[]) throws IOException {
+        List<ModeloAtividade> modeloAtividadeList = new ArrayList<ModeloAtividade>();
 
-        GenericParser parser = new GenericParser(input);
+        modeloAtividadeList.add(new ModeloAtividade("Teste 01", new DateTime(2016,01,01,0,0),new DateTime(2016,02,01,0,0)));//DateTime(Ano,Mes,Dia,Hora,Minuto)
+        modeloAtividadeList.add(new ModeloAtividade("Teste 02",64, new DateTime(2016,01,01,0,0),new DateTime(2016,06,01,0,0)));
+        modeloAtividadeList.add(new ModeloAtividade("Teste 03",128, new DateTime(2015,01,01,0,0),new DateTime(2016,06,01,0,0)));
+        modeloAtividadeList.add(new ModeloAtividade("Teste 04", new DateTime(2015,01,01,0,0),new DateTime(2015,06,01,0,0)));
 
-        try {
-            parser.GenerateTXT();
-        } catch(IOException ex) {
-            System.out.println("Io Exception");
-        }
-
+        ConfeccionaDocumento.popularDocumento(modeloAtividadeList);
     }
 }
---
+++ src/main/java/ms/escritor/ConfeccionaDocumento.java
@@ -0,0 +1,40 @@
+package ms.escritor;
+
+import ms.manipulador.ModeloAtividade;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by Bruno Messias on 08/07/2016.
+ */
+public class ConfeccionaDocumento {
+
+    /**
+     * Este método recebe a lista de atividades, e publica-as, ordenando-as por parágrafo.
+     * O arquivo é gerado na pasta raiz do projeto
+     * @param modeloAtividadeList
+     * @throws IOException
+     */
+    public static void popularDocumento(List<ModeloAtividade> modeloAtividadeList) throws IOException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try{
+            FileWriter atividadesExtraídas = new FileWriter(new File((System.getProperty("user.dir")),"AtividadesExtraídas.txt"));
+            PrintWriter printWriter = new PrintWriter(atividadesExtraídas);
+            for(ModeloAtividade atividade :modeloAtividadeList) {
+                printWriter.println("Id da Atividade = "+atividade.getSequencialAtividade());
+                printWriter.println(atividade.getDescricaoAtividade());
+                printWriter.println("Quantidade de horas = "+atividade.getQtdeHorasAtividade());
+                printWriter.println("Data de inicio = "+  simpleDateFormat.format(atividade.getDtInicioAtividade().toDate()));
+                printWriter.println("Data de fim = "+ simpleDateFormat.format(atividade.getDtFimAtividade().toDate()));
+                printWriter.println();
+            }
+            atividadesExtraídas.close();
+        } catch(IOException e){
+            System.out.println("ConfeccionaDocumento" + e.getMessage());
+        }
+    }
+}
