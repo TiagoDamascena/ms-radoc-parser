a584e71ea8f289a71054cf32cc116972f26337e2
TiagoDamascena <tiagobdamascena@gmail.com>
2016-07-23 16:30:25 -0300

    Criado extrator de codigos e pontuação do CONSUNI

--- src/main/java/ms/leitor/LeitorAtividadesConsuni.java
+++ src/main/java/ms/leitor/LeitorAtividadesConsuni.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import ms.modelo.AtividadeConsuni;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -17,15 +17,14 @@ import org.jdom2.input.SAXBuilder;
  */
 public class LeitorAtividadesConsuni {
 
-    private static LeitorAtividadesConsuni la = new LeitorAtividadesConsuni();
     /**
      *
      * @return
      * @throws JDOMException
      * @throws IOException
      */
-    public static int searchWord(String atividadeProcurada) throws JDOMException, IOException, URISyntaxException {
-        ClassLoader classLoader = la.getClass().getClassLoader();
+    public AtividadeConsuni buscarAtividade(String atividadeProcurada) throws JDOMException, IOException, URISyntaxException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
         URI uri = classLoader.getResource("Atividades.xml").toURI();
         File file = new File(uri);
 
@@ -37,56 +36,39 @@ public class LeitorAtividadesConsuni {
         List elements = categories.getChildren();
         Iterator i = elements.iterator();
 
-        List<Theme> themes = new ArrayList<>();
-
         while(i.hasNext()){
 
             Element category = (Element)i.next();
-            Theme theme = new Theme(category.getAttributeValue("nome"));
-            List<Element> children = category.getChildren();
 
-            for (Element atividade : children){
-                if(atividadeProcurada.equals(atividade)){
-                    return 0;
+            if(atividadeProcurada.equals(category.getAttributeValue("nome"))) {
+               return montarModelo(category);
+            } else {
+                AtividadeConsuni atividadeConsuni = procuraFilhos(category, atividadeProcurada);
+                if(atividadeConsuni != null) {
+                    return atividadeConsuni;
                 }
-                else {
-                    List<Element> tipo = atividade.getChildren();
-                    if(!tipo.isEmpty()){
-                        for(Element categoria : tipo){
-                            if(atividadeProcurada.equals(tipo)){
-                                return 0;
-                            }
-                        }
-                    }
-                    theme.addWord(word.getText());
-                }
-                themes.add(theme);
-
-
             }
-
-
-            return themes;
         }
+        return null;
     }
 
-    public void procuraFilhos(Element element, String atividadeProcurada){
+    private AtividadeConsuni procuraFilhos(Element element, String atividadeProcurada){
         List<Element> childrens = element.getChildren();
 
         if(!childrens.isEmpty()){
             for (Element children : childrens){
                 if(atividadeProcurada.equals(children.getAttributeValue("nome"))){
-                    montaCodigo(children);
-                    return;
+                    return montarModelo(children);
                 }
                 else {
-                    procuraFilhos(children, atividadeProcurada);
+                    return procuraFilhos(children, atividadeProcurada);
                 }
             }
         }
+        return null;
     }
 
-    public String montaCodigo(Element element){
+    private String montaCodigo(Element element){
         String cod = new String();
         cod = element.getAttributeValue("valor");
 
@@ -99,5 +81,15 @@ public class LeitorAtividadesConsuni {
                 return cod;
             }
         }
+    }
+
+    private AtividadeConsuni montarModelo(Element element) {
+        AtividadeConsuni atividade = new AtividadeConsuni();
+        atividade.setCodigo(montaCodigo(element));
+        Element pontos = element.getChild("pontos");
+        if(pontos != null) {
+            atividade.setPontos(pontos.getValue());
+        }
+        return atividade;
     }
 }
---
+++ src/main/java/ms/modelo/AtividadeConsuni.java
@@ -0,0 +1,25 @@
+package ms.modelo;
+
+/**
+ * Created by Tiago on 23/07/2016.
+ */
+public class AtividadeConsuni {
+    private String codigo;
+    private String pontos;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(String pontos) {
+        this.pontos = pontos;
+    }
+}
